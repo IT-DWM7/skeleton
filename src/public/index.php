38a5b3bf0fbf40098bc74a46385258097895db1b
sferey <s.ferey@cleo-consulting.fr>
2017-05-19 12:32:13 +0200

    Première Skeleton

---
+++ src/public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Box Tatum</title>
+</head>
+<body>
+    <h1>Hello world !!!</h1>
+    <p>Bienvenu sur votre Tatum edition spécial limité DWM7</p>
+    <p>Si ce n'est pas déjà fait, pensez à mettre à jour le fichier hosts 
+        de votre système hôte en ajoutant la ligne suivante : </p> 
+    <pre>
+        Windows : "C:\Windows\System32\drivers\etc\hosts"<br>
+        Linux & OSX : "/etc/hosts"<br>
+        <code>192.168.33.10  www.dwm7.dev</code>
+    </pre> 
+</body>
+</html>
